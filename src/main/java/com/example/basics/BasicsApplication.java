47f54ff884b9c228ee68f48cf1b7f6af3ee4cdc1
Shushant <profshushant@gmail.com>
2025-01-14 21:34:35 +0530

    learned how to create a springboot project from scratch

--- src/main/java/com/example/basics/BasicsApplication.java
+++ src/main/java/com/example/basics/BasicsApplication.java
@@ -1,13 +1,19 @@
 package com.example.basics;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class BasicsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicsApplication.class, args);
+//        SpringApplication.run(BasicsApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(BasicsApplication.class);
+        springApplication.run();
     }
 
 }
