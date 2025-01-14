9f04cf3aef62b685592b8257e1cfd5a2c9dfd53c
Shushant <profshushant@gmail.com>
2025-01-14 22:21:15 +0530

    revisited basics part 2:

--- src/main/java/com/example/basics/BasicsApplication.java
+++ src/main/java/com/example/basics/BasicsApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
+//@EnableAutoConfiguration
+//@ComponentScan
 public class BasicsApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(BasicsApplication.class, args);
-        SpringApplication springApplication = new SpringApplication(BasicsApplication.class);
-        springApplication.run();
+        SpringApplication.run(BasicsApplication.class, args); // as run is a static method
+//        SpringApplication springApplication = new SpringApplication(BasicsApplication.class);
+//        springApplication.run();
     }
 
 }
