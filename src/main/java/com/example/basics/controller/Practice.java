dbb6fdebce8e208d851cdd1fc733bdde829c8f5b
Shushant <profshushant@gmail.com>
2025-01-17 14:40:07 +0530

    made my REST apis

---
+++ src/main/java/com/example/basics/controller/Practice.java
@@ -0,0 +1,44 @@
+package com.example.basics.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class Practice {
+
+    private List<String> foods = new ArrayList<>();
+
+    @GetMapping
+    public List<String> getFoods() {
+        return foods;
+    }
+
+    @PostMapping("/add/{food}")
+    public String addFood(@PathVariable String food) {
+        foods.add(food);
+        return "Added " + food;
+    }
+
+    @DeleteMapping("/del/{food}")
+    public String  delFood(@PathVariable String food) {
+        foods.remove(food);
+        return "Deleted " + food;
+    }
+
+
+    @PutMapping("/update/{name}/{newName}")
+    public String  updateName(@PathVariable String name, @PathVariable String newName) {
+            int index = foods.indexOf(name);
+            if(index != -1) {
+                foods.set(index, newName);
+            }
+            return "Updated " + newName;
+    }
+
+
+
+
+}
